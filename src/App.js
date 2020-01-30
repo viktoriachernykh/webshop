79f512d0b80a2a19aefef2d55d028d70a2a89eb2
viktoriachernykh <chernycv@gmail.com>
2020-01-30 11:03:51 +0100

    json and reducer files  added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom"; // adding router
+import CataloguePage from "./components/CataloguePage";
+import CheckoutPage from "./components/CheckoutPage"; // for router
+import Toolbar from "./components/Toolbar";
+import PaymentPage from "./components/PaymentPage";
+// import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Toolbar />
+        <Switch>
+          <Route path="/catalogue" component={CataloguePage} />
+          <Route path="/checkout" component={CheckoutPage} />
+          <Route path="/checkout" component={PaymentPage} />
+          {/* <Route component={Homepage} /> */}
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/store/catalogue/reducer.js
@@ -0,0 +1,14 @@
+// const initialState = null;
+
+// export default function reducer(state = initialState, action) {
+//   switch (action.type) {
+//     case "developers/FETCHED": {
+//       // console.log("action.payload from developers/FETCHED", action.payload); // { count: 105, rows: [] }
+//       // console.log("state from developers/FETCHED", state); // array with 10 objects
+//       return action.payload;
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// }
